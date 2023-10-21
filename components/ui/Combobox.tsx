30c68ee4f5b9151278af2b670f297f489a9e07a5
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-21 17:27:23 +0400

    add ui

---
+++ components/ui/Combobox.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import * as React from "react";
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ComboboxProp } from "@/types";
+
+export function ComboboxDemo({
+  options,
+  defaultLabel,
+  handleSelectValue,
+  setOpen,
+  value,
+  open,
+}: ComboboxProp) {
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between hover:bg-white/60"
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : defaultLabel || "Select option..."}
+          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] border-white/20 bg-white/60 p-0">
+        <Command className="bg-white/40">
+          {/* <CommandInput placeholder="Search option..." className="h-9" /> */}
+          <CommandEmpty>No option found.</CommandEmpty>
+          <CommandGroup>
+            {options.map((option) => (
+              <CommandItem
+                key={option.value}
+                onSelect={() => handleSelectValue(option.value)}
+              >
+                {option.label}
+                <CheckIcon
+                  className={cn(
+                    "ml-auto h-4 w-4",
+                    value === option.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
