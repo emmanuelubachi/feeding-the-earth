c844a96c98dbc6df266d7e182ef2bb6aa7c5a28c
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-27 23:55:38 +0400

    add constants

---
+++ constants/index.ts
@@ -0,0 +1,13 @@
+import { YearProps, navbar } from "@/types";
+
+export const NAVITEMS: navbar = [
+  { id: 1, name: "Country", link: "/countries" },
+  { id: 2, name: "Data", link: "/" },
+];
+
+export const years: YearProps = [
+  { value: 2018, label: "2018" },
+  { value: 2017, label: "2017" },
+  { value: 2016, label: "2016" },
+  { value: 2015, label: "2015" },
+];
