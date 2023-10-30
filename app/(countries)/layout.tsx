b93e95bd165a6d56c43bb7d056b0a24c80e296ad
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-30 14:59:41 +0400

    update layout

--- app/(countries)/layout.tsx
+++ app/(countries)/layout.tsx
@@ -8,11 +8,11 @@ const layout = ({ children }: { children: React.ReactNode }) => {
       <LeftSidebar />
 
       <section
-        className="flex min-h-screen flex-1 flex-col 
+        className="m-auto flex min-h-screen flex-1 flex-col items-center justify-center overflow-hidden 
             p-6 max-md:pb-14 
             sm:px-14"
       >
-        <div className="mx-auto w-full max-w-5xl">{children}</div>
+        <div className="mx-auto w-full ">{children}</div>
       </section>
     </main>
   );
