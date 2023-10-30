ec2c654323480e85b99710c962c9d218dfffb772
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-30 15:02:36 +0400

    update sidebar component

--- components/shared/sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/LeftSidebar.tsx
@@ -2,31 +2,26 @@
 import Link from "next/link";
 import React from "react";
 
-import { AnimatePresence, motion } from "framer-motion";
-import { menuSlide } from "./anim";
+// import { AnimatePresence, motion } from "framer-motion";
+// import { menuSlide } from "./anim";
 
 const LeftSidebar = () => {
   return (
     <>
-      <AnimatePresence mode="wait">
-        <motion.div
-          variants={menuSlide}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          className="hidden h-screen flex-col overflow-hidden 
-        bg-orange-50
-        p-6 
+      <div
+        className="fixed z-10 hidden h-screen flex-col overflow-hidden 
+        bg-orange-50 p-6 
         shadow-2xl md:flex
         md:w-72 lg:w-[400px]"
-        >
-          <Link href="/">Back to Introduction </Link>
-          <br />
-          <Link href="/countries">Countries</Link>
-          <br />
-          <Link href="/heatmap">Heatmap</Link>
-        </motion.div>
-      </AnimatePresence>
+      >
+        <Link href="/">Back to Introduction </Link>
+        <br />
+        <Link href="/countries">Countries</Link>
+        <br />
+        <Link href="/heatmap">Heatmap</Link>
+        <br />
+        <Link href="/map">Map</Link>
+      </div>
     </>
   );
 };
