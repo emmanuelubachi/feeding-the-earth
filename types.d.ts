acb41d11ec5ae24b01a191a9e36aa91c9e4a8d56
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-30 14:59:08 +0400

    update types

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,19 @@
-export type YearProps = {
-  label: string;
-  value: number;
+export type navbar = {
+  id: number;
+  name: string;
+  link: string;
 }[];
+
+// Charts props -----
+export type ChartParamProps = {
+  name: string;
+  value: number;
+};
 
 export type DataProps = {
   name: string;
   value: number;
+  ISO_3?: string;
 }[];
 
 export type DatasetProps = {
@@ -15,9 +23,16 @@ export type DatasetProps = {
   Data: DataProps;
 }[];
 
+// Filter props ----
+export type YearProps = {
+  label: string;
+  value: number;
+}[];
+
 export type YearFilterProps = {
-  dataset: DatasetProps;
+  dataset?: DatasetProps;
   filter: YearProps;
+  defaultChart: "Treemap" | "Bar" | "Line" | "Map";
 };
 
 export type ComboboxProp = {
@@ -31,9 +46,3 @@ export type ComboboxProp = {
   value: any;
   open: boolean;
 };
-
-export type navbar = {
-  id: number;
-  name: string;
-  link: string;
-}[];
